b57c44e8537c6bcdf0a38b3d279469b7a8214b1b
Jayd Saucedo <jayd.saucedo@nerdery.com>
2016-02-01 16:38:33 -0600

    Creates simple pager that downloads data from spotify

---
+++ Thundercloud/app/src/main/java/boombotix/com/thundercloud/ui/activity/PlayerActivity.java
@@ -0,0 +1,57 @@
+package boombotix.com.thundercloud.ui.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import boombotix.com.thundercloud.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class PlayerActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawer;
+//    @Bind(R.id.nav_list)
+//    ListView navList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        ArrayList<String> items = new ArrayList<String>(){{add("test1"); add("yep2"); add("Hello3");}};
+//        navList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, items));
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
