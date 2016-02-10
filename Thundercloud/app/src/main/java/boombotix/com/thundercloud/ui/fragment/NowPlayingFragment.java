c61358801db44316ecddf12416ce6b80a801cb15
Theo Kanning <tkanning@nerdery.com>
2016-02-10 17:59:55 -0600

    Now playing bar changes, Added default background

--- Thundercloud/app/src/main/java/boombotix/com/thundercloud/ui/fragment/NowPlayingFragment.java
+++ Thundercloud/app/src/main/java/boombotix/com/thundercloud/ui/fragment/NowPlayingFragment.java
@@ -1,17 +1,14 @@
 package boombotix.com.thundercloud.ui.fragment;
 
 
+import com.jesusm.holocircleseekbar.lib.HoloCircleSeekBar;
+
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.SeekBar;
 import android.widget.TextView;
-
-import com.jesusm.holocircleseekbar.lib.HoloCircleSeekBar;
 
 import boombotix.com.thundercloud.R;
 import boombotix.com.thundercloud.ui.activity.MainActivity;
@@ -21,10 +18,13 @@ import butterknife.ButterKnife;
 
 public class NowPlayingFragment extends BaseFragment implements
         HoloCircleSeekBar.OnCircleSeekBarChangeListener {
+
     @Bind(R.id.picker)
     HoloCircleSeekBar picker;
+
     @Bind(R.id.progress_text)
     TextView progressText;
+
     @Bind(R.id.play_button)
     ImageButton playButton;
 
@@ -46,7 +46,7 @@ public class NowPlayingFragment extends BaseFragment implements
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_now_playing, container, false);
         ButterKnife.bind(this, view);
         picker.setOnSeekBarChangeListener(this);
