58dc4c32fe3d0ad0acc45fe8d277af40cd24e2d4
Jayd Saucedo <jayd.saucedo@nerdery.com>
2016-02-16 17:25:45 -0600

    sets up timed query

--- Thundercloud/app/src/main/java/boombotix/com/thundercloud/ui/fragment/VoiceSearchResultFragment.java
+++ Thundercloud/app/src/main/java/boombotix/com/thundercloud/ui/fragment/VoiceSearchResultFragment.java
@@ -1,6 +1,7 @@
 package boombotix.com.thundercloud.ui.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -15,6 +16,7 @@ import com.hound.android.sdk.TextSearch;
 
 import javax.inject.Inject;
 
+import boombotix.com.thundercloud.BuildConfig;
 import boombotix.com.thundercloud.R;
 import boombotix.com.thundercloud.houndify.HoundifyHelper;
 import boombotix.com.thundercloud.houndify.model.HoundifyResponse;
@@ -22,6 +24,8 @@ import boombotix.com.thundercloud.ui.activity.TopLevelActivity;
 import boombotix.com.thundercloud.ui.base.BaseFragment;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnEditorAction;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -38,6 +42,14 @@ public class VoiceSearchResultFragment extends BaseFragment {
     TextView queryText;
     @Bind(R.id.voice_search_result_edit)
     EditText editText;
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            doSearch(String.valueOf(queryText.getText()));
+        }
+    };
 
     public VoiceSearchResultFragment() {
     }
@@ -61,6 +73,12 @@ public class VoiceSearchResultFragment extends BaseFragment {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
+    @Override
     public void onDestroyView() {
         ButterKnife.unbind(this);
         ((TopLevelActivity) getActivity()).stopPlayerSearch();
@@ -73,34 +91,50 @@ public class VoiceSearchResultFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_voice_search_result, container, false);
         ButterKnife.bind(this, view);
 
-        editText.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                TextSearch textSearch = new TextSearch.Builder()
-                        .setClientId(PlayerFragment.CLIENT_ID)
-                        .setClientKey(PlayerFragment.CLIENT_KEY)
-                        .setRequestInfo(houndifyHelper.getHoundRequestInfo(getContext()))
-                        .setQuery(String.valueOf(v.getText()))
-                        .build();
-
-                Observable.defer(() -> {
-                    try {
-                        return Observable.just(textSearch.search());
-                    } catch (TextSearch.TextSearchException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                }).subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(result -> {
-                            HoundifyResponse houndifyResponse =  houndifyHelper.parseResponse(result.getResponse());
-                            Toast.makeText(getContext(),houndifyResponse.getTracks().get(0).getTrackName(), Toast.LENGTH_LONG).show();
-                        });
-                return true;
-            }
-            return false;
-        });
 
         return view;
+    }
+
+    @OnClick(R.id.voice_search_result_text)
+    public void onVoiceSearchResultClick(View v) {
+        timerHandler.removeCallbacks(timerRunnable);
+        tapToEdit.setVisibility(View.GONE);
+        queryText.setVisibility(View.GONE);
+        editText.setVisibility(View.VISIBLE);
+    }
+
+    @OnEditorAction(R.id.voice_search_result_edit)
+    public boolean onQueryEdit(TextView v, int actionId, KeyEvent event){
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            doSearch(String.valueOf(v.getText()));
+            return true;
+        }
+        return false;
+    }
+
+
+    private void doSearch(String q) {
+       TextSearch textSearch = new TextSearch.Builder()
+                            .setRequestInfo(houndifyHelper.getHoundRequestInfo(getContext()))
+                            .setClientId(BuildConfig.HOUNDIFY_CLIENT_ID)
+                            .setClientKey(BuildConfig.HOUNDIFY_CLIENT_KEY)
+                            .setQuery(q)
+                            .build();
+
+
+        Observable.defer(() -> {
+            try {
+                return Observable.just(textSearch.search());
+            } catch (TextSearch.TextSearchException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> {
+                    HoundifyResponse houndifyResponse =  houndifyHelper.parseResponse(result.getResponse());
+                    Toast.makeText(getContext(),houndifyResponse.getTracks().get(0).getTrackName(), Toast.LENGTH_LONG).show();
+                });
     }
 
     public void updateText(String s){
@@ -108,17 +142,10 @@ public class VoiceSearchResultFragment extends BaseFragment {
     }
 
     public void setQuery(String s){
+        timerHandler.post(timerRunnable);
         queryText.setText(s);
         tapToEdit.setVisibility(View.VISIBLE);
         editText.setText(s);
-        queryText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tapToEdit.setVisibility(View.GONE);
-                queryText.setVisibility(View.GONE);
-                editText.setVisibility(View.VISIBLE);
-            }
-        });
     }
 
 }
